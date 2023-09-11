3524440048534d608932abdddaa2c13a9ae73468
sylviaOsinaku <osinakusylvia@gmail.com>
2023-09-11 16:34:02 +0100

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZDBhYTU5MTU0YzQ5MDgwMGU1OGM4ZmFjZGRjOTY2YyIsInN1YiI6IjY0NzliOGU2ZTMyM2YzMDBhN2Q0ZTcxZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fg3OBEqMzOlMwGwkHJZO7Un-ReTWYVt9LIp3gP0dfhE",
+    },
+  };
+
+  fetch("https://api.themoviedb.org/3/authentication", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuto1Ude7eUTNVJWk0nNOHzAOvvOicdOY",
+  authDomain: "movieblaze-65c5d.firebaseapp.com",
+  projectId: "movieblaze-65c5d",
+  storageBucket: "movieblaze-65c5d.appspot.com",
+  messagingSenderId: "931001902443",
+  appId: "1:931001902443:web:5f40983872d5484404982e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
